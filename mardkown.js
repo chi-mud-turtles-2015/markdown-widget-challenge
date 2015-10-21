a72b0c7920a636aca228d19f606be373e7bcff53
Linas Mikalcius & Sheena Bryant <pair+linas1985.shebryant@devbootcamp.com>
2015-10-21 15:17:24 -0500

    add MVC

--- mardkown.js
+++ mardkown.js
@@ -1,29 +1,46 @@
-function MarkdownView(){
-  console.log("HELLO");
-  this.getInputBox = function (){
-    return $("input");
-  }
-  this.getInputContent = function (){
-    return $("input").val();
-  }
-  this.transformOutput = function(output){
-    $("p").text(output);
-  }
+function MarkdownView(options) {
+  this.input = $('input');
+  this.display = $('p');
+  // console.log(this.display);
+}
+
+MarkdownView.prototype.bindKeypress = function(callback) {
+  this.input.on('keypress', callback);
+}
+
+MarkdownView.prototype.getInputContent = function (){
+  return this.input.val();
+}
+
+MarkdownView.prototype.transformOutput = function(output){
+  this.display.text(output);
+}
+// MarkdownView.prototype.getInputBox = function (){
+//   console.log("HELLO");
+//   return this.input;
+// }
+MarkdownController.prototype.onKeypress = function(event) {
+  console.log(this);
+  console.log(event.target);
+  this.text.rawText = this.view.getInputContent();
+  console.log("HELLLLLOO");
+  this.view.transformOutput(this.text.transformedText());
 }
 
 function MarkdownController(){
-  var view = new MarkdownView();
-  var text = new Text();
-  view.getInputBox().on("keyup", function(){
-    text.rawText = view.getInputContent();
-    view.transformOutput(text.transformedText());
-  });
+  // console.log("HELLO");
+  this.view = new MarkdownView();
+  this.text = new Text();
+  this.view.bindKeypress(this.onKeypress.bind(this))
 }
-
 
 function Text(){
   this.rawText = "";
-  this.transformedText = function(){
-    return this.rawText + "****";
-  }
 }
+Text.prototype.transformedText = function(){
+  return "****" + this.rawText + "****"
+}
+
+$(function(){
+  var controller = new MarkdownController();
+});
