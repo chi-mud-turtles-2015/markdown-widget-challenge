b15736a8f7fe3416582a680dd36e4683f35407ce
brianmock <briannealmock@gmail.com>
2015-10-21 14:54:48 -0500

    Move logic out of model, into viewmodel

--- markdown-widget/app/assets/javascripts/views/text.js
+++ markdown-widget/app/assets/javascripts/views/text.js
@@ -14,8 +14,12 @@ TextView.prototype = {
   },
 
   checkChar: function(char, shift) {
-    str = this.model.checkSpecialChar(char,shift);
+    str = this.checkSpecialChar(char,shift);
+    // console.log(str);
     var selector = $('.markdown');
+    // console.log(this.model.special);
+    // console.log(this.model.bold);
+    // console.log(this.model.italics);
     if (this.model.special === false) {
       if (this.model.bold === true) {
         selector = $('.markdown span:last-child');
@@ -28,7 +32,83 @@ TextView.prototype = {
       this.removeMarkdown();
     }
     else {
+      // console.log(selector);
       this.addMarkdown(selector,str);
+    }
+  },
+
+  checkSpecialChar: function(char,shift){
+    if (char == 16) {
+      return '';
+    }
+    if (char == 32) {
+      return ' ';
+    }
+    if (char == 8) {
+      return 'backspace';
+    }
+    if (char == 13) {
+      return '<br>';
+    }
+    if (shift === true) {
+      if (char == 56) {
+        char = this.doStar(char);
+      }
+      else if (char == 189) {
+        char = this.doUnderscore(char);
+      }
+      else {
+        this.starCount = 0;
+        char = String.fromCharCode(char);
+      }
+      return char;
+    }
+    else {
+      this.model.starCount = 0;
+      this.model.special = false;
+      if (char == 56 || char == 189) {
+        return null;
+      }
+      else {
+        return String.fromCharCode(char);
+      }
+    }
+  },
+
+  doUnderscore: function(char) {
+    this.model.starCount = 0;
+    this.model.special = true;
+    this.model.italics = !this.model.italics;
+    if (this.model.italics === true) {
+      return '<em>';
+    }
+    else {
+      return '';
+    }
+  },
+
+  doStar: function(char) {
+    this.model.special = true;
+    if (this.model.starCount == 1) {
+      this.model.bold = !this.model.bold;
+      this.model.italics = false;
+      this.model.starCount = 0;
+      if (this.model.bold === true) {
+        return '<span class="bold">';
+      }
+      else {
+        return '';
+      }
+    }
+    else if (this.model.starCount === 0) {
+      this.model.italics = !this.model.italics;
+      this.model.starCount += 1;
+      if (this.model.italics === true) {
+        return '<em>';
+      }
+      else {
+        return '';
+      }
     }
   },
 
